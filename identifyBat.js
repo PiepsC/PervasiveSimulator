a42d0e741e9e80b8357f7980611bc5b788710f77
rohith2597 <54556355+rohith2597@users.noreply.github.com>
2021-06-12 00:47:57 +0200

    Update identifyBat.js
    Made few changes now server can able to receive the bytes and also store the image

--- identifyBat.js
+++ identifyBat.js
@@ -1,38 +1,37 @@
-const sharp = require('sharp');
+const fs = require('fs');
 var http = require('http');
 var str = '';
- 
-process_image()
 
-async function process_image(){
-    let binData = await sharp(String.raw`images\bat-${process.argv[4]}.jpg`) //Change to forward slash on linux systems
-        .raw()
-        .toBuffer({ resolveWithObject: false }); //Sharp is brilliant at reformatting the images if necessary
-        
-        try{
-            var request = http.request({
-                method: 'POST',
-                port: 5000,
-                host: `${process.argv[2]}`,
-                path: `${process.argv[3]}`,
-                headers : {'Content-Type' : 'image/jpeg',
-                           'Content-Length': binData.length}
-            });
+(function () {
+  const imgBinary = Buffer.from(
+    fs.readFileSync(String.raw`images\bat-${process.argv[4]}.jpg`)
+  );
 
-            request.on('response', function(res) {
-                res.on('data', (chunk) => {
-                    str += chunk;
-                })
-                res.on('end', () =>{
-                    console.log(str)
-                })
-            });
+  try {
+    var request = http.request({
+      method: 'POST',
+      port: 5000,
+      host: `${process.argv[2]}`,
+      path: `${process.argv[3]}`,
+      headers: {
+        'Content-Type': 'image/jpeg',
+        'Content-Length': imgBinary.length,
+      },
+    });
 
-            request.write(binData);
-            request.end()
-    } catch(err) {
-        console.error("look an error lol" + err);
-    }
-    return;
-}
+    request.on('response', function (res) {
+      res.on('data', (chunk) => {
+        str += chunk;
+      });
+      res.on('end', () => {
+        console.log(str);
+      });
+    });
+    request.write(imgBinary);
+    request.end();
+  } catch (err) {
+    console.error('look an error lol' + err);
+  }
+  return;
+})();
 
